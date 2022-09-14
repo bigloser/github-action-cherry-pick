7c74bdf799f78b63cf64b83084dbc93b664ebcdf
Sumit Shah <sumit@columntax.com>
2022-09-14 12:06:03 -0700

    add debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -42,6 +42,6 @@ git_cmd git remote update
 git_cmd git fetch --all
 git_cmd git checkout -b "${PR_BRANCH}" origin/"${INPUT_PR_BRANCH}"
 git_cmd git log -n 2
-git_cmd git cherry-pick "${GITHUB_SHA}"
+git_cmd git cherry-pick --allow-empty "${GITHUB_SHA}"
 git_cmd git push -u origin "${PR_BRANCH}"
 git_cmd hub pull-request -b "${INPUT_PR_BRANCH}" -h "${PR_BRANCH}" -l "${INPUT_PR_LABELS}" -a "${GITHUB_ACTOR}" -m "\"AUTO: ${PR_TITLE}\""
